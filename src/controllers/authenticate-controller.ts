8b0b7eaf690ba2b99ca0aa52a1a7275507b0370d
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-05-10 13:44:24 +0930

    auth controller

--- src/controllers/authenticate-controller.ts
+++ src/controllers/authenticate-controller.ts
@@ -1,34 +1,54 @@
 import {
   Body,
-  ConflictException,
   Controller,
-  HttpCode,
   Post,
+  UnauthorizedException,
   UsePipes,
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
-import { hash } from 'bcryptjs'
+import { compare } from 'bcryptjs'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
-// const createAccountBodySchema = z.object({
-//   name: z.string(),
-//   email: z.string().email(),
-//   password: z.string().min(6),
-// })
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
 
-// type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
 
   @Post()
   // @HttpCode(201)
-  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handle() {
-    const token = this.jwt.sign({ sub: 'user-id' })
-    return token
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    const isValidPassword = await compare(password, user.password)
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid credentials')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return { access_token: accessToken }
   }
 }
