b11abbe2a6486415ccf33ead553625cb91aa2685
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-07-29 15:10:08 +0930

    questions e2e test

---
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -0,0 +1,72 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+
+import request from 'supertest'
+
+describe('Fetch Recent Questions (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'j@j.com',
+        password: '123456',
+      },
+    })
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: 'title test 1',
+          slug: 'title-test-1',
+          content: 'content test',
+          authorId: user.id,
+        },
+        {
+          title: 'title test 2',
+          slug: 'title-test-2',
+          content: 'content test',
+          authorId: user.id,
+        },
+      ],
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({
+          title: 'title test 1',
+        }),
+        expect.objectContaining({
+          title: 'title test 2',
+        }),
+      ],
+    })
+  })
+})
