930f5b9997284d1121bc28379c36e6c40cb644ad
Felipe Makoto <felipe.makoto@e-train.com.au>
2024-07-30 14:06:58 +0930

    create question mapper

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaClient } from '@prisma/client'
+
+export class PrismaQuestionMApper {
+  static toDomain(raw: PrismaClient): Question {
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -0,0 +1,52 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+import { PrismaQuestionMApper } from '../mappers/prisma-question-mapper'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMApper.toDomain(question)
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = await this.prisma.question.findFirst({
+      where: {
+        slug,
+      },
+    })
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMApper.toDomain(question)
+  }
+
+  async findManyRecent(params: PaginationParams): Promise<Question[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async save(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async create(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+
+  async delete(question: Question): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
